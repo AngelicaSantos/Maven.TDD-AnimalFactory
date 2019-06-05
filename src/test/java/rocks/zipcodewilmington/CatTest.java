d53317e009ad33d0a76954fbbdd86c0a0701d713
AngelicaSantos <angelica.santos2113@gmail.com>
2019-06-05 09:24:18 -0400

    complete

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -10,13 +10,6 @@ import java.util.Date;
  * @author leon on 4/19/18.
  */
 public class CatTest {
-    // TODO - Create tests for `void setName(String name)`
-    // TODO - Create tests for `speak`
-    // TODO - Create tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
-    // TODO - Create tests for `Integer getId()`
-    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
-    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
 
 
     @Test
@@ -39,5 +32,75 @@ public class CatTest {
         Assert.assertEquals(givenBirthDate, retrievedBirthDate);
         Assert.assertEquals(givenId, retrievedId);
     }
+
+    // TODO - Create tests for `void setName(String name)
+
+    @Test
+    public void setNameTest() {
+
+        String expected = "lulu";
+        Cat myCat = new Cat(null,null,null);
+        myCat.setName(expected);
+        String actual = myCat.getName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    // TODO - Create tests for `speak`
+
+    @Test
+    public void setSpeakTest() {
+        String expected = "meow!";
+        Cat myCat = new Cat(null,null,null);
+        String actual = myCat.speak();
+        Assert.assertEquals(expected,actual);
+    }
+
+    // TODO - Create tests for `void eat(Food food)`
+    @Test
+    public void setEatTest() {
+
+        // Given (a cat that eats)
+        Cat cat = new Cat(null, null, null);
+        Food catFood = new Food();
+        int givenNumberOfMealsEaten = cat.getNumberOfMealsEaten();
+
+
+        // When(the cat eats)
+        cat.eat(catFood);
+        int retrieveNumberOfMealsEaten = cat.getNumberOfMealsEaten();
+
+
+        // Then (we expect the cat to eat food)
+        Assert.assertEquals(givenNumberOfMealsEaten + 1, retrieveNumberOfMealsEaten);
+    }
+
+    // TODO - Create tests for `Integer getId()`
+
+    @Test
+    public void getIdTest(){
+        // Given (a cat that has an ID)
+        int givenId = 2468;
+        Cat cat = new Cat(null, null, givenId);
+
+
+        // When(we retrieve the ID)
+        int retrieveCatId = cat.getId();
+
+
+        // Then (we expect to get the ID we set for the cat)
+        Assert.assertEquals( givenId , retrieveCatId);
+    }
+
+// TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
+
+
+
+
+
+
+
+    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+
+
 
 }
--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,5 +1,6 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
@@ -29,6 +30,86 @@ public class DogHouseTest {
         DogHouse.add(animal);
 
         // Then
-        DogHouse.getNumberOfDogs();
+        int retrievedNumberOfDogs = DogHouse.getNumberOfDogs();
+        Assert.assertEquals(1, retrievedNumberOfDogs);
     }
+
+
+    @Test
+    public void addTest() {
+        // Given (some
+        Dog dog = AnimalFactory.createDog(null, null);
+
+
+        // When
+        DogHouse.add(dog);
+        Dog dog2Point0 = DogHouse.getDogById(dog.getId());
+
+        // Then
+        Assert.assertEquals(dog, dog2Point0);
+
+
+    }
+
+    @Test
+    public void removeByIdTest(){
+        //Given
+        Dog animal = AnimalFactory.createDog(null, null);
+        Integer animalId = animal.getId();
+
+
+        //When
+        DogHouse.add(animal);
+        DogHouse.remove(animalId);
+        Dog animal2Point0 = DogHouse.getDogById(animal.getId());
+
+
+        //Then
+        Assert.assertEquals(null, animal2Point0);
+
+
+    }
+
+    @Test
+    public void removeByDogTest(){
+        //Given
+        Dog animal = AnimalFactory.createDog(null, null);
+
+
+        //When
+        DogHouse.add(animal);
+        DogHouse.remove(animal);
+        Dog animal2Point0 = DogHouse.getDogById(animal.getId());
+
+
+        //Then
+        Assert.assertEquals(null, animal2Point0);
+
+
+    }
+
+
+    @Test
+    public void getDogByIdtest(){
+        //Given
+        DogHouse.clear();
+        Dog animal = AnimalFactory.createDog(null , null);
+
+        //When
+        int dogHouseSize = DogHouse.getNumberOfDogs();
+        DogHouse.add(animal);
+        int retreivedId = animal.getId();
+
+
+        //Then
+        Assert.assertEquals(dogHouseSize, retreivedId);
+
+
+    }
+
+
+
+
+
+
 }
